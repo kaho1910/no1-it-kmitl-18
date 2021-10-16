338dbe33b86ab7d36eb31ad7c0464091286bd333
Trda092 <73659917+Trda092@users.noreply.github.com>
2021-10-16 17:29:13 +0700

    create prototype of ETM pages

--- src/pages/Entertainment.js
+++ src/pages/Entertainment.js
@@ -1,5 +1,8 @@
 import React from "react";
 import DocTitle from "../components/etc/DocTitle";
+import f_etm from "../img/img_etm/f_etm.png";
+import ui_etm from "../img/img_etm/ui_etm.png";
+import "../pages/Entertainment.css";
 
 function Entertainment() {
     DocTitle(
@@ -7,15 +10,25 @@ function Entertainment() {
     );
     return (
         <div>
-        <figure className="position-relative">
-        <img className="img-fluid" src={f_etm} alt="Ps5etm" width="100%"/>
-        <figcaption className="text-left caption">
+        <div className="layer banner position-relative">
+        <img className="img-fluid fetm" src={f_etm} alt="Ps5etm" width="100%"/>
+        <div className="banner-content">
+        <div className="caption">
             <h1>ความบันเทิงบน PS5</h1>
             <p>สตรีมรายการทีวี ภาพยนตร์ และดนตรีที่ยอดเยี่ยมจากบริการด้านความบันเทิงที่คุณโปรดปราน</p>
             <br></br>
-            <p>บริการสตรีมมิ่งอาจต้องมีการสมัครสมาชิกแบบมีค่าใช้จ่าย เนื้อหาที่แสดงในหน้านี้อาจไม่พร้อมใช้งานในทุกประเทศ ต้องมีอินเทอร์เน็ตและบัญชีสำหรับ PlayStation™Network ด้วย</p>
-        </figcaption>
-        </figure>
+            <p className="llt">บริการสตรีมมิ่งอาจต้องมีการสมัครสมาชิกแบบมีค่าใช้จ่าย เนื้อหาที่แสดงในหน้านี้อาจไม่พร้อมใช้งานในทุกประเทศ ต้องมีอินเทอร์เน็ตและบัญชีสำหรับ PlayStation™Network ด้วย</p>
+        </div>
+        </div>
+        </div>
+        <br></br>
+        <div className="layer seclayer">
+            <h2>ทีวีและภาพยนตร์บน PlayStation®5</h2>
+            <p style={{opacity:0.7}}>พบกับรายการทีวีโปรดรายการถัดไปของคุณ สตรีมเนื้อหาวิดีโอล่าสุด หรือเตรียมตัวรับชมภาพยนตร์</p>
+            <div className="ui_layout">
+                <img src={ui_etm} alt="Ps5ui" width="56%" />
+            </div>
+        </div>
     </div>
     );
 }
