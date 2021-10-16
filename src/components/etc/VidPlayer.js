6b11181ab77d33d6cf7c2240e68e0fdf32371f2f
Thirawee Tawan <thirawee.tawan@gmail.com>
2021-10-16 21:49:05 +0700

    feat: intro to VidPlayer.js

---
+++ src/components/etc/VidPlayer.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./VidPlayer.css";
+
+function VidPlayer() {
+    return (
+        <div className="video-player w-full h-full m-4 rounded-3xl cursor-pointer transition duration-300 ease-in-out bg-red-500">
+            {/* <img src="" className="w-full h-full" alt="" /> */}
+        </div>
+    );
+}
+
+export default VidPlayer;
