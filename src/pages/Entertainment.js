84e72a8cd2b5ea094c9d10fc77354fea1812c0c3
Trda092 <73659917+Trda092@users.noreply.github.com>
2021-10-16 17:30:52 +0700

    fix bug

--- src/pages/Entertainment.js
+++ src/pages/Entertainment.js
@@ -1,11 +1,10 @@
 import React from "react";
 import DocTitle from "../components/etc/DocTitle";
 import f_etm from "../img/img_etm/f_etm.png";
-<<<<<<< HEAD
 import ui_etm from "../img/img_etm/ui_etm.png";
 import "../pages/Entertainment.css";
-=======
->>>>>>> 2aaf986fbd3dadb9e7a18622687d6d7ad3802ecb
+
+
 
 function Entertainment() {
     DocTitle(
@@ -13,7 +12,6 @@ function Entertainment() {
     );
     return (
         <div>
-<<<<<<< HEAD
         <div className="layer banner position-relative">
         <img className="img-fluid fetm" src={f_etm} alt="Ps5etm" width="100%"/>
         <div className="banner-content">
@@ -34,31 +32,6 @@ function Entertainment() {
             </div>
         </div>
     </div>
-=======
-            <figure className="position-relative">
-                <img
-                    className="img-fluid"
-                    src={f_etm}
-                    alt="Ps5etm"
-                    width="100%"
-                />
-                <figcaption className="text-left caption">
-                    <h1>ความบันเทิงบน PS5</h1>
-                    <p>
-                        สตรีมรายการทีวี ภาพยนตร์
-                        และดนตรีที่ยอดเยี่ยมจากบริการด้านความบันเทิงที่คุณโปรดปราน
-                    </p>
-                    <br></br>
-                    <p>
-                        บริการสตรีมมิ่งอาจต้องมีการสมัครสมาชิกแบบมีค่าใช้จ่าย
-                        เนื้อหาที่แสดงในหน้านี้อาจไม่พร้อมใช้งานในทุกประเทศ
-                        ต้องมีอินเทอร์เน็ตและบัญชีสำหรับ PlayStation™Network
-                        ด้วย
-                    </p>
-                </figcaption>
-            </figure>
-        </div>
->>>>>>> 2aaf986fbd3dadb9e7a18622687d6d7ad3802ecb
     );
 }
 
