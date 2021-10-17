51843dccbe37c6e91df125eb5fc611d84e6620fb
Thirawee Tawan <thirawee.tawan@gmail.com>
2021-10-17 14:54:34 +0700

    fix: BtnBlue .active bg-color

--- src/components/etc/BtnBlue.js
+++ src/components/etc/BtnBlue.js
@@ -3,9 +3,7 @@ import "./VidBtn.css";
 
 const BtnBlue = (props) => {
     return (
-        <button
-            className="vid-btn-blue text-white border-white text-lg p-3 pr-6 pl-6 border-2 rounded-full transition ease-in duration-100"
-            style={{ backgroundColor: "#2d64e6" }}>
+        <button className="vid-btn-blue text-white border-white text-lg p-3 pr-6 pl-6 border-2 rounded-full transition ease-in duration-100">
             <b className="inline-block">{props.txt}</b>
         </button>
     );
