806c4d1d81609ee0e8f0989865626a327be1a7d4
Thirawee Tawan <thirawee.tawan@gmail.com>
2021-10-17 12:08:09 +0700

    fix: VidBtnBlue .active bg-color

--- src/components/etc/VidBtnBlue.js
+++ src/components/etc/VidBtnBlue.js
@@ -4,9 +4,7 @@ import "./VidBtn.css";
 
 function VidBtnBlue() {
     return (
-        <button
-            className="vid-btn-blue text-white border-white text-lg p-3 pr-6 pl-6 border-2 rounded-full transition ease-in duration-100"
-            style={{ backgroundColor: "#2d64e6" }}>
+        <button className="vid-btn-blue text-white border-white text-lg p-3 pr-6 pl-6 border-2 rounded-full transition ease-in duration-100">
             <img src={arrow} className="arrow-blue inline-block" alt="arrow" />
             <b className="inline-block">ดูวิดีโอ</b>
         </button>
