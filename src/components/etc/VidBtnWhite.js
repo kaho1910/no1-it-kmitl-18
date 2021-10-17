4c58351674ec1b7c4248abb7d25bcdd9aedd03a6
Thirawee Tawan <thirawee.tawan@gmail.com>
2021-10-17 15:50:43 +0700

    fix: BtnBlue, BtnWhite, VidBtnBlue, VidBtnWhite border

--- src/components/etc/VidBtnWhite.js
+++ src/components/etc/VidBtnWhite.js
@@ -4,7 +4,9 @@ import "./VidBtn.css";
 
 function VidBtnWhite() {
     return (
-        <button className="vid-btn-white text-black bg-white text-lg p-3 pr-6 pl-6 border-2 border-black rounded-full transition ease-in duration-100">
+        <button
+            className="vid-btn-white bg-white text-black border-transparent bg-clip-padding text-lg p-2 px-5 rounded-full transition ease-in duration-100"
+            style={{ borderWidth: "3px" }}>
             <img src={arrow} className="arrow-white inline-block" alt="arrow" />
             <b className="inline-block">ดูวิดีโอ</b>
         </button>
