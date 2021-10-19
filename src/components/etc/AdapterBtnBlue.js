5db537bbdcc2d2891b3317418785d14494a9696a
pawpawmasterpiece <92322108+pawpawmasterpiece@users.noreply.github.com>
2021-10-19 20:39:18 +0700

    Add files via upload

---
+++ src/components/etc/AdapterBtnBlue.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./VidBtn.css";
+
+function LearnMoreBtnBlue() {
+    return (
+        <button className="vid-btn-blue text-white border-white text-lg p-3 pr-6 pl-6 border-2 rounded-full transition ease-in duration-100">
+            <b className="inline-block">ขออะแดปเตอร์ PlayStation Camera เลย</b>
+        </button>
+    );
+}
+
+export default LearnMoreBtnBlue;
