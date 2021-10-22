d5dbc2f7225ded1a50efbc8da06dce5722b186d7
Thirawee Tawan <thirawee.tawan@gmail.com>
2021-10-22 21:37:54 +0700

    fix: increase MainBtn's transition duration

--- src/components/button/MainBtnWhite.js
+++ src/components/button/MainBtnWhite.js
@@ -3,7 +3,7 @@ import "./VidBtn.css";
 
 const BtnWhite = (props) => {
     return (
-        <button className="bg-white text-black text-xl p-2 px-8 rounded-full transition ease-in duration-100 hover:bg-black hover:text-white">
+        <button className="bg-white text-black text-xl p-2 px-8 rounded-full transition ease-in duration-200 hover:bg-black hover:text-white">
             <b className="inline-block">{props.txt}</b>
         </button>
     );
