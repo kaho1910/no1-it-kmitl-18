10df80730259bbd813f0d7bcf845bfe93a0874be
Thirawee Tawan <thirawee.tawan@gmail.com>
2021-10-22 22:05:52 +0700

    refactor: text styling

--- src/components/button/MainBtnBlue.js
+++ src/components/button/MainBtnBlue.js
@@ -3,8 +3,8 @@ import "./VidBtn.css";
 
 const BtnWhite = (props) => {
     return (
-        <button className="main-btn-blue text-white text-xl p-2 px-8 rounded-full transition ease-in duration-200 hover:bg-white">
-            <b className="inline-block">{props.txt}</b>
+        <button className="main-btn-blue text-white p-2 px-8 rounded-full transition ease-in duration-200 hover:bg-white">
+            <p className="inline-block  text-xl font-semibold">{props.txt}</p>
         </button>
     );
 };
