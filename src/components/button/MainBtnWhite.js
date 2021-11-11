0c297f83a18574d4a198cadfbc1da808781c700a
Thirawee Tawan <thirawee.tawan@gmail.com>
2021-11-11 20:40:26 +0700

    style: font weight semibold to normal

--- src/components/button/MainBtnWhite.js
+++ src/components/button/MainBtnWhite.js
@@ -4,7 +4,7 @@ import "./VidBtn.css";
 const BtnWhite = (props) => {
     return (
         <button className="bg-white text-black p-2 px-8 rounded-full transition ease-in duration-200 hover:bg-black hover:text-white">
-            <p className="inline-block text-xl font-semibold">{props.txt}</p>
+            <p className="inline-block text-xl">{props.txt}</p>
         </button>
     );
 };
