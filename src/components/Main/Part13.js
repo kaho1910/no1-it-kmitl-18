3818c7ee6c1081709cee6cdf9f82ff0407139b56
Thirawee Tawan <thirawee.tawan@gmail.com>
2021-11-11 12:29:39 +0700

    refactor: refactor

--- src/components/Main/Part13.js
+++ src/components/Main/Part13.js
@@ -1,7 +1,7 @@
 import React, { Component, useRef } from "react";
 import Slider from "react-slick";
 import "./styles.css";
-import "slick-carousel/slick/slick.css"; 
+import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 import MainBtnWhite from "../button/MainBtnWhite";
 import img1 from "../../img/Main/Part13/01.jpg";
@@ -25,7 +25,7 @@ function Part13() {
         centerPadding: "50px",
         slidesToShow: 5,
         speed: 500,
-      };
+    };
     return (
         <div
             className="w-full overflow-x-hidden"
@@ -34,100 +34,141 @@ function Part13() {
                 backgroundColor: "#002fc3",
                 backgroundImage: "linear-gradient(180deg,#0931b7,#082eaa)",
             }}>
-				<Slider {...settings}>
-					<div className="box">
-					  <img className="img-card" src={img1} />
-            <p className="title">Call of Duty: Vanguard</p>
-            <p className="cont">นี่คือการต่อสู้ในสงครามโลกครั้งที่สองในระดับโลกที่ไม่เคยมีมาก่อน ผงาดในทุกแนวรบในขณะที่คุณมีบทบาทสำคัญในการเปลี่ยนแปลงโฉมหน้าของประวัติศาสตร์</p>
-            <div className="card-btn text-left mt-5 h-3">
-            <MainBtnWhite txt="เรียนรู้เพิ่มเติม"/>
-            </div>
-          </div>
-					<div className="box">
-            <img className="img-card" src={img2}/>
-            <p className="title">EA SPORTS FIFA 22</p>
-            <p className="cont">เทคโนโลยี HyperMotion ใหม่ที่ปฏิวัติวงการใช้พลังของ PlayStation 5 ในการยกระดับทุกช่วงเวลาในสนาม เพื่อปลดล็อคการเล่นเกมที่สมจริงที่สุดในประวัติศาสตร์ของ FIFA</p>
-            <div className="card-btn text-left mt-5 h-3">
-            <MainBtnWhite txt="เรียนรู้เพิ่มเติม"/>
-            </div>
-          </div>
-					<div className="box">
-						<img className="img-card" src={img3}/>
-            <p className="title">Resident Evil: Village</p>
-            <p className="cont">สัมผัสประสบการณ์เกมเอาตัวรอดสยองขวัญที่ไม่เหมือนครั้งไหนๆ กับเกมภาคหลักลำดับที่แปดของตระกูล Resident Evil ที่มาในชื่อ Resident Evil Village</p>
-            <div className="card-btn text-left mt-5 h-3">
-            <MainBtnWhite txt="เรียนรู้เพิ่มเติม"/>
-            </div>
-					</div>
-					<div className="box">
-						<img className="img-card" src={img4}/>
-            <p className="title">Demon's Souls</p>
-            <p className="cont">เกมรีเมคที่ถูกสร้างขึ้นมาใหม่ทั้งหมด เชื้อเชิญคุณมาสัมผัสกับประสบการณ์การต่อสู้ที่โหดเหี้ยม และเรื่องราวที่เต็มไปด้วยปริศนาในเกม Demon's Souls</p>
-            <div className="card-btn text-left mt-5 h-3">
-            <MainBtnWhite txt="เรียนรู้เพิ่มเติม"/>
-            </div>
-					</div>
-					<div className="box">
-						<img className="img-card" src={img5}/>
-            <p className="title">Ratchet & Clank: Rift Apart</p>
-            <p className="cont">บุกตะลุยในการผจญภัยข้ามมิติไปกับ Ratchet and Clank ในการต่อสู้กับจักรพรรดิผู้ชั่วร้ายจากอีกโลกหนึ่ง</p>
-            <div className="card-btn text-left mt-5 h-3">
-            <MainBtnWhite txt="เรียนรู้เพิ่มเติม"/>
-            </div>
-					</div>
-					<div className="box">
-						<img className="img-card" src={img6}/>
-            <p className="title">Marvel's Spider-Man: Miles Morales</p>
-            <p className="cont">พบกับการปรากฏตัวของ Miles Morales ในฐานะฮีโร่ผู้เก่งกาจคนใหม่ที่มาพร้อมพลังพิเศษอันน่าเหลือเชื่อเต็มพิกัดเพื่อเป็น Spider-Man ในแบบของเขาเอง</p>
-            <div className="card-btn text-left mt-5 h-3">
-            <MainBtnWhite txt="เรียนรู้เพิ่มเติม"/>
-            </div>
-					</div>
-          <div className="box">
-						<img className="img-card" src={img7}/>
-            <p className="title">ฮอไรซันฟอร์บิดเดนเวสต์: ผจญภัยสุดเขตแดนต้องห้าม</p>
-            <p className="cont">ร่วมผจญภัยไปกับเอลอย ฝ่าฟันฟอร์บิดเนเวสต์ พรมแดนอันยิ่งใหญ่ที่ซ่อนเร้นไปด้วยภัยอันตรายลึกลับใหม่ๆ</p>
-            <div className="card-btn text-left mt-5 h-3">
-            <MainBtnWhite txt="เรียนรู้เพิ่มเติม"/>
-            </div>
-					</div>
-          <div className="box">
-						<img className="img-card" src={img8}/>
-            <p className="title">Returnal</p>
-            <p className="cont">ทำลายวงโคจรแห่งความโกลาหลบนดวงดาวเอเลี่ยนสุดประหลาดที่สภาพแวดล้อมเปลี่ยนแปลงอยู่ตลอดเวลา กับเกมชูตเตอร์แบบโรกผ่านมุมมองบุคคลที่สามจากผู้พัฒนา Housemarque</p>
-            <div className="card-btn text-left mt-5 h-3">
-            <MainBtnWhite txt="เรียนรู้เพิ่มเติม"/>
-            </div>
-					</div>
-          <div className="box">
-						<img className="img-card" src={img9}/>
-            <p className="title">DEATHLOOP</p>
-            <p className="cont">สวมบทบาทเป็น Colt เพื่อค้นหาวิธีที่จะยุติวังวนที่กักขังเขาไว้บนเกาะ Blackreef ขณะถูกคนบนเกาะตามไล่ฆ่า</p>
-            <div className="card-btn text-left mt-5 h-3">
-            <MainBtnWhite txt="เรียนรู้เพิ่มเติม"/>
-            </div>
-					</div>
-          <div className="box">
-						<img className="img-card" src={img10}/>
-            <p className="title">NBA 2K22</p>
-            <p className="cont">NBA 2K22 นำจักรวาลแห่งบาสเก็ตบอลทั้งหมดมาไว้ในมือคุณแล้ว PLAY NOW ในสภาพแวดล้อมจริงของ NBA และ WNBA กับทีมและผู้เล่นจริง สร้างทีมในฝันของคุณเองใน MyTEAM ด้วยผู้เล่นดาวเด่นในปัจจุบันและผู้เล่นระดับตำนานในอดีต</p>
-            <div className="card-btn text-left mt-5 h-3">
-            <MainBtnWhite txt="เรียนรู้เพิ่มเติม"/>
-            </div>
-					</div>
-          <div className="box">
-						<h3 className="hide">11</h3>
-					</div>
-          <div className="box">
-						<h3 className="hide">12</h3>
-					</div>
-          <div className="box">
-						<h3 className="hide">11</h3>
-					</div>
-          <div className="box">
-						<h3 className="hide">12</h3>
-					</div>
-				</Slider>
+            <Slider {...settings}>
+                <div className="box">
+                    <img className="img-card" src={img1} alt="slick" />
+                    <p className="title">Call of Duty: Vanguard</p>
+                    <p className="cont">
+                        นี่คือการต่อสู้ในสงครามโลกครั้งที่สองในระดับโลกที่ไม่เคยมีมาก่อน
+                        ผงาดในทุกแนวรบในขณะที่คุณมีบทบาทสำคัญในการเปลี่ยนแปลงโฉมหน้าของประวัติศาสตร์
+                    </p>
+                    <div className="card-btn text-left mt-5 h-3">
+                        <MainBtnWhite txt="เรียนรู้เพิ่มเติม" />
+                    </div>
+                </div>
+                <div className="box">
+                    <img className="img-card" src={img2} alt="slick" />
+                    <p className="title">EA SPORTS FIFA 22</p>
+                    <p className="cont">
+                        เทคโนโลยี HyperMotion ใหม่ที่ปฏิวัติวงการใช้พลังของ
+                        PlayStation 5 ในการยกระดับทุกช่วงเวลาในสนาม
+                        เพื่อปลดล็อคการเล่นเกมที่สมจริงที่สุดในประวัติศาสตร์ของ
+                        FIFA
+                    </p>
+                    <div className="card-btn text-left mt-5 h-3">
+                        <MainBtnWhite txt="เรียนรู้เพิ่มเติม" />
+                    </div>
+                </div>
+                <div className="box">
+                    <img className="img-card" src={img3} alt="slick" />
+                    <p className="title">Resident Evil: Village</p>
+                    <p className="cont">
+                        สัมผัสประสบการณ์เกมเอาตัวรอดสยองขวัญที่ไม่เหมือนครั้งไหนๆ
+                        กับเกมภาคหลักลำดับที่แปดของตระกูล Resident Evil
+                        ที่มาในชื่อ Resident Evil Village
+                    </p>
+                    <div className="card-btn text-left mt-5 h-3">
+                        <MainBtnWhite txt="เรียนรู้เพิ่มเติม" />
+                    </div>
+                </div>
+                <div className="box">
+                    <img className="img-card" src={img4} alt="slick" />
+                    <p className="title">Demon's Souls</p>
+                    <p className="cont">
+                        เกมรีเมคที่ถูกสร้างขึ้นมาใหม่ทั้งหมด
+                        เชื้อเชิญคุณมาสัมผัสกับประสบการณ์การต่อสู้ที่โหดเหี้ยม
+                        และเรื่องราวที่เต็มไปด้วยปริศนาในเกม Demon's Souls
+                    </p>
+                    <div className="card-btn text-left mt-5 h-3">
+                        <MainBtnWhite txt="เรียนรู้เพิ่มเติม" />
+                    </div>
+                </div>
+                <div className="box">
+                    <img className="img-card" src={img5} alt="slick" />
+                    <p className="title">Ratchet & Clank: Rift Apart</p>
+                    <p className="cont">
+                        บุกตะลุยในการผจญภัยข้ามมิติไปกับ Ratchet and Clank
+                        ในการต่อสู้กับจักรพรรดิผู้ชั่วร้ายจากอีกโลกหนึ่ง
+                    </p>
+                    <div className="card-btn text-left mt-5 h-3">
+                        <MainBtnWhite txt="เรียนรู้เพิ่มเติม" />
+                    </div>
+                </div>
+                <div className="box">
+                    <img className="img-card" src={img6} alt="slick" />
+                    <p className="title">Marvel's Spider-Man: Miles Morales</p>
+                    <p className="cont">
+                        พบกับการปรากฏตัวของ Miles Morales
+                        ในฐานะฮีโร่ผู้เก่งกาจคนใหม่ที่มาพร้อมพลังพิเศษอันน่าเหลือเชื่อเต็มพิกัดเพื่อเป็น
+                        Spider-Man ในแบบของเขาเอง
+                    </p>
+                    <div className="card-btn text-left mt-5 h-3">
+                        <MainBtnWhite txt="เรียนรู้เพิ่มเติม" />
+                    </div>
+                </div>
+                <div className="box">
+                    <img className="img-card" src={img7} alt="slick" />
+                    <p className="title">
+                        ฮอไรซันฟอร์บิดเดนเวสต์: ผจญภัยสุดเขตแดนต้องห้าม
+                    </p>
+                    <p className="cont">
+                        ร่วมผจญภัยไปกับเอลอย ฝ่าฟันฟอร์บิดเนเวสต์
+                        พรมแดนอันยิ่งใหญ่ที่ซ่อนเร้นไปด้วยภัยอันตรายลึกลับใหม่ๆ
+                    </p>
+                    <div className="card-btn text-left mt-5 h-3">
+                        <MainBtnWhite txt="เรียนรู้เพิ่มเติม" />
+                    </div>
+                </div>
+                <div className="box">
+                    <img className="img-card" src={img8} alt="slick" />
+                    <p className="title">Returnal</p>
+                    <p className="cont">
+                        ทำลายวงโคจรแห่งความโกลาหลบนดวงดาวเอเลี่ยนสุดประหลาดที่สภาพแวดล้อมเปลี่ยนแปลงอยู่ตลอดเวลา
+                        กับเกมชูตเตอร์แบบโรกผ่านมุมมองบุคคลที่สามจากผู้พัฒนา
+                        Housemarque
+                    </p>
+                    <div className="card-btn text-left mt-5 h-3">
+                        <MainBtnWhite txt="เรียนรู้เพิ่มเติม" />
+                    </div>
+                </div>
+                <div className="box">
+                    <img className="img-card" src={img9} alt="slick" />
+                    <p className="title">DEATHLOOP</p>
+                    <p className="cont">
+                        สวมบทบาทเป็น Colt
+                        เพื่อค้นหาวิธีที่จะยุติวังวนที่กักขังเขาไว้บนเกาะ
+                        Blackreef ขณะถูกคนบนเกาะตามไล่ฆ่า
+                    </p>
+                    <div className="card-btn text-left mt-5 h-3">
+                        <MainBtnWhite txt="เรียนรู้เพิ่มเติม" />
+                    </div>
+                </div>
+                <div className="box">
+                    <img className="img-card" src={img10} alt="slick" />
+                    <p className="title">NBA 2K22</p>
+                    <p className="cont">
+                        NBA 2K22 นำจักรวาลแห่งบาสเก็ตบอลทั้งหมดมาไว้ในมือคุณแล้ว
+                        PLAY NOW ในสภาพแวดล้อมจริงของ NBA และ WNBA
+                        กับทีมและผู้เล่นจริง สร้างทีมในฝันของคุณเองใน MyTEAM
+                        ด้วยผู้เล่นดาวเด่นในปัจจุบันและผู้เล่นระดับตำนานในอดีต
+                    </p>
+                    <div className="card-btn text-left mt-5 h-3">
+                        <MainBtnWhite txt="เรียนรู้เพิ่มเติม" />
+                    </div>
+                </div>
+                <div className="box">
+                    <h3 className="hide">11</h3>
+                </div>
+                <div className="box">
+                    <h3 className="hide">12</h3>
+                </div>
+                <div className="box">
+                    <h3 className="hide">11</h3>
+                </div>
+                <div className="box">
+                    <h3 className="hide">12</h3>
+                </div>
+            </Slider>
         </div>
     );
 }
